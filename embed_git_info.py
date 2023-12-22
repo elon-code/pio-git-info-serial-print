8c134381b95f98abd014fa4765e9af6e5ca73a45
Elon Goliger Mallimson <egoliger@exploratorium.edu>
2023-12-21 16:13:15 -0800

    modify so it works with powershell instead of unix

--- embed_git_info.py
+++ embed_git_info.py
@@ -1,21 +1,26 @@
 import subprocess
+import os
+
+print("Embed Git Info Script has started")
 
 def get_git_info():
     commands = {
-        'tag': "git describe --tags",
+        'tag': "git describe --tags --always",  # use --always flag to avoid failure when no tags exist
         'branch': "git rev-parse --abbrev-ref HEAD",
         'commit_hash': "git rev-parse HEAD",
         'date': "git log -1 --format=%cd",
         'author': "git log -1 --format=%cn",
         'author_email': "git log -1 --format=%ce",
         'repo_url': "git config --get remote.origin.url",
-        'repo_name': "basename `git rev-parse --show-toplevel`"
+        'repo_name': "echo %cd%",  # use Windows equivalent of basename command
     }
 
     info = {}
     for key, command in commands.items():
         try:
             result = subprocess.check_output(command, shell=True).decode().strip()
+            if key == 'repo_name':
+                result = os.path.basename(result)  # extract the last part of the path
             info[key] = result
         except subprocess.CalledProcessError:
             info[key] = "Unknown"
