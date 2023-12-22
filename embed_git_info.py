7c06275599ab37909b6893f18a5ccd4850a30587
Elon Goliger Mallimson <egoliger@exploratorium.edu>
2023-12-22 14:56:28 -0800

    feat(embedGitInfo): enhance git info gathering and incorporate CRC calculation
    In this commit, we have enhanced the script for embedding Git information. It now gathers more detailed
    information including local Git user details, build date and time, and
    repository name derived from the origin URL. In
    addition to this, unnecessary imports are removed,
    and exception handling has been improved.
    
    Also, a significant new feature is the addition of a CRC32 checksum calculation for a specified file. This checksum is written to the generated header file along with the collected Git information.

--- embed_git_info.py
+++ embed_git_info.py
@@ -1,49 +1,76 @@
+# Importing the environment from PlatformIO and necessary modules
 Import("env")
 
-try:
-    import subprocess
-    import os
-except ImportError:
-    env.Execute("$PYTHONEXE -m pip install subprocess")
-    env.Execute("$PYTHONEXE -m pip install os")
+import subprocess
+import os
+import zlib  # For CRC calculation
+import datetime
 
 print("Embed Git Info Script has started")
 
+# Function to gather git, system, and build information
 def get_git_info():
-    # Dictionary of git commands to extract various information
-    commands = {
-        'tag': "git describe --tags --always",
-        'branch': "git rev-parse --abbrev-ref HEAD",
-        'commit_hash': "git rev-parse HEAD",
-        'date': "git log -1 --format=%cd",
-        'author': "git log -1 --format=%cn",
-        'author_email': "git log -1 --format=%ce",
-        'repo_url': "git config --get remote.origin.url",
-        'repo_name': os.path.basename(os.getcwd()),
+    # Commands to extract various information
+    commands = { # Comment out undesirable variables to save on storage
+        'git_tag': "git describe --tags --always",
+        'git_branch': "git rev-parse --abbrev-ref HEAD",
+        'git_commit_hash': "git rev-parse HEAD",
+        # 'git_date': "git log -1 --format=%cd", # I believe build date is better then git date. Git date can be found with commit hash
+        # 'git_author': "git log -1 --format=%cn", # These are optional variables, they are not really necessary in printout
+        # 'git_author_email': "git log -1 --format=%ce", # These are optional variables, they are not really necessary in printout
+        'git_repo_url': "git config --get remote.origin.url",
+        'git_repo_name': "git config --get remote.origin.url | xargs basename -s .git",
+        'local_user_name': "git config user.name",
+        'local_user_email': "git config user.email",
+        # 'folder_name': os.path.basename(os.getcwd()), # I feel git repo name is better, but may choose otherwise
+        # 'computer_user': os.getlogin(), # I feel git user is better, but you may choose computer username
+        # Adding build date and time
+        'build_date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), # date of build
     }
 
+    # Executing commands and collecting results
     info = {}
     for key, command in commands.items():
         try:
-            if key == 'repo_name':
+            if key in ['build_date', 'build_time', 'folder_name']:
+                # Directly using values for certain keys
                 result = commands[key]
             else:
+                # Executing shell commands for Git information
                 result = subprocess.check_output(command, shell=True).decode().strip()
             info[key] = result
         except subprocess.CalledProcessError:
+            # Handling command execution errors
             info[key] = "Unknown"
 
     return info
 
-def write_header(info):
+# Function to calculate the CRC of a given file
+def calculate_crc(filename):
+    try:
+        with open(filename, 'rb') as file:
+            data = file.read()
+            return zlib.crc32(data)
+    except Exception as e:
+        return f"Error calculating CRC: {e}"
+
+# Function to write the gathered information into a header file
+def write_header(info, crc):
     header_path = os.path.join(env['PROJECT_DIR'], 'include', 'git_info.h')
     with open(header_path, 'w') as f:
         for key, value in info.items():
-            f.write(f"#define GIT_{key.upper()} \"{value}\"\n")
+            # Writing each piece of information as a macro definition
+            f.write(f"#define {key.upper()} \"{value}\"\n")
+        # Writing CRC information
+        f.write(f"#define MAIN_FILE_CRC {crc}\n")
 
+
+# Main execution block
 try:
     git_info = get_git_info()
-    write_header(git_info)
+    # Replace with the actual path to your main file
+    main_file_crc = calculate_crc('path/to/your/main/file')  
+    write_header(git_info, main_file_crc)
 except Exception as e:
     print(f"An error occurred: {e}")
 
