585103f7368450de898d20b4019ad281a9f1665b
Elon Goliger Mallimson <egoliger@exploratorium.edu>
2024-01-25 15:25:35 -0800

    update readme and example print to remove crc

--- ExamplePrint.cpp
+++ ExamplePrint.cpp
@@ -1,12 +1,13 @@
  
    // This is the recommended setup, you may choose other setups
+// Note: The F macro is used to store the string in flash memory, which is where the Arduino stores the sketch. This is to save RAM.
+// however, it only does stuff for AVR boards, so it's not necessary for ARM boards. It doesn't hurt to use it though.
 Serial.print(F("Git Information:\n"));
 Serial.print(F("Build Date/Time (local time): ")); Serial.print(BUILD_DATE); Serial.print(F("\n"));
 Serial.print(F("Builder's Name: ")); Serial.print(GIT_USER_NAME); Serial.print(F(" Email: ")); Serial.print(GIT_USER_EMAIL); Serial.print(F("\n"));
 Serial.print(F("Repository URL: ")); Serial.print(GIT_REPO_URL); Serial.print(F("\n"));
 Serial.print(F("Branch: ")); Serial.print(GIT_BRANCH); Serial.print(F(" | Tag: ")); Serial.print(GIT_TAG); Serial.print(F("\n\n")); // Optional, only use if using tags.
 Serial.print(F("Commit Hash: ")); Serial.print(GIT_COMMIT_HASH); Serial.print(F("\n"));
-Serial.print(F("CRC: ")); Serial.println(static_cast<long>(MAIN_FILE_CRC));
 Serial.print(F("\n=================================\n"));
 
   // These are optional. Not really necessary as it's easy to see who worked on repo by going to link.
@@ -21,5 +22,4 @@ Serial << F("Git Information:\n")
   << F("Repository URL: ") << GIT_REPO_URL << F("\n")
   << F("Branch: ") << GIT_BRANCH << F(" | Tag: ") << GIT_TAG  << F("\n\n")
   << F("Commit Hash: ") << GIT_COMMIT_HASH << F("\n") 
-  << F("CRC: ") << static_cast<long>(MAIN_FILE_CRC) 
   << F("\n=================================") << endl;
